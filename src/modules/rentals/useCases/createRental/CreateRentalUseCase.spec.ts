632502fbaa262ef8e75b98a60d242c0b3537e2a4
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-07-20 18:53:31 -0300

    changing createRentalUseCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -3,10 +3,12 @@ import dayjs from 'dayjs';
 import { DayjsDateProvider } from '../../../../shared/container/providers/DateProvider/implementations/DayjsDateProvider';
 import { AppError } from '../../../../shared/errors/AppError';
 import { CarsRespositoryInMemory } from '../../../cars/repositories/in-memory/CarsRepositoryInMemory';
+import { CreateCarUseCase } from '../../../cars/useCases/createCar/CreateCarUseCase';
 import { RentalsRepositoryInMemory } from '../../repositories/in-memory/RentalsRepositoryInMemory';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let createRentalUseCase: CreateRentalUseCase;
+let createCarUseCase: CreateCarUseCase;
 let rentalsRepositoryinMemory: RentalsRepositoryInMemory;
 let carsRepositoryInMemory: CarsRespositoryInMemory;
 let dayjsDateProvider: DayjsDateProvider;
@@ -18,6 +20,7 @@ describe('Create rental', () => {
     rentalsRepositoryinMemory = new RentalsRepositoryInMemory();
     carsRepositoryInMemory = new CarsRespositoryInMemory();
     dayjsDateProvider = new DayjsDateProvider();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryinMemory,
       dayjsDateProvider,
@@ -26,9 +29,19 @@ describe('Create rental', () => {
   });
 
   it('should be able to create a new rental', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car',
+      description: 'This is a fucking car',
+      daily_rate: 100,
+      license_plate: 'ABC-1212',
+      fine_amount: 60,
+      brand: 'brand',
+      category_id: 'category',
+    });
+
     const rental = await createRentalUseCase.execute({
       user_id: '12345',
-      car_id: '54546',
+      car_id: car.id,
       expected_return_date: dayAdd24Hours,
     });
 
@@ -38,15 +51,25 @@ describe('Create rental', () => {
 
   it('should not be able to create a new rental if there is another open rental to the same user', () => {
     expect(async () => {
+      const car = await createCarUseCase.execute({
+        name: 'Car',
+        description: 'This is a fucking car',
+        daily_rate: 100,
+        license_plate: 'ABC-1212',
+        fine_amount: 60,
+        brand: 'brand',
+        category_id: 'category',
+      });
+
       await createRentalUseCase.execute({
         user_id: '12345',
-        car_id: '545959',
+        car_id: car.id,
         expected_return_date: dayAdd24Hours,
       });
 
       await createRentalUseCase.execute({
         user_id: '12345',
-        car_id: '54546',
+        car_id: '65165',
         expected_return_date: dayAdd24Hours,
       });
     }).rejects.toBeInstanceOf(AppError);
@@ -54,15 +77,25 @@ describe('Create rental', () => {
 
   it('should not be able to create a new rental if there is another open rental to the same car', () => {
     expect(async () => {
+      const car = await createCarUseCase.execute({
+        name: 'Car',
+        description: 'This is a fucking car',
+        daily_rate: 100,
+        license_plate: 'ABC-1212',
+        fine_amount: 60,
+        brand: 'brand',
+        category_id: 'category',
+      });
+
       await createRentalUseCase.execute({
         user_id: '12345',
-        car_id: '54546',
+        car_id: car.id,
         expected_return_date: dayAdd24Hours,
       });
 
       await createRentalUseCase.execute({
         user_id: '123455',
-        car_id: '54546',
+        car_id: car.id,
         expected_return_date: dayAdd24Hours,
       });
     }).rejects.toBeInstanceOf(AppError);
@@ -70,9 +103,19 @@ describe('Create rental', () => {
 
   it('should not be able to create a new rental with invalid return time', async () => {
     expect(async () => {
+      const car = await createCarUseCase.execute({
+        name: 'Car',
+        description: 'This is a fucking car',
+        daily_rate: 100,
+        license_plate: 'ABC-1212',
+        fine_amount: 60,
+        brand: 'brand',
+        category_id: 'category',
+      });
+
       await createRentalUseCase.execute({
         user_id: '12345',
-        car_id: '54546',
+        car_id: car.id,
         expected_return_date: dayjs().toDate(),
       });
     }).rejects.toBeInstanceOf(AppError);
