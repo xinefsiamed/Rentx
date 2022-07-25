c75b32b193681b6d5ab2ca31bae4ae311baad615
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-07-25 20:35:14 -0300

    adding refreshTokenUseCase

---
+++ src/modules/accounts/useCases/RefreshToken/RefreshTokenController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+class RefreshTokenController {
+  async handle(req: Request, res: Response): Promise<Response> { }
+}
+
+export { RefreshTokenController };
---
+++ src/modules/accounts/useCases/RefreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,60 @@
+import { sign, verify } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import auth from '../../../../config/auth';
+import { DayjsDateProvider } from '../../../../shared/container/providers/DateProvider/implementations/DayjsDateProvider';
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUsersTokensRepository } from '../../repositories/IUsersTokensRepository';
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+@injectable()
+class RefresTokenUseCase {
+  constructor(
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject('DayjsDateProvider')
+    private dateProvider: DayjsDateProvider
+  ) { } //eslint-disable-line
+
+  async execute(token: string) {
+    const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
+
+    const user_id = sub;
+
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token
+      );
+
+    if (!userToken) {
+      throw new AppError('Refresh token does not exists.');
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, auth.secret_refresh_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_refresh_token,
+    });
+
+    const expires_date = this.dateProvider.addDays(
+      auth.experies_refresh_token_days
+    );
+
+    await this.usersTokensRepository.create({
+      user_id,
+      expires_date,
+      refresh_token,
+    });
+
+    return refresh_token;
+  }
+}
+
+export { RefresTokenUseCase };
